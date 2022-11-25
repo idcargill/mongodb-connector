8be05c1a24d385527f796b90fd5cd55bbb3673d6
idcargill <Idcargill@gmail.com>
2022-11-25 15:06:45 -0800

    update tests

--- src/MongoDBConnector.ts
+++ src/MongoDBConnector.ts
@@ -1,11 +1,13 @@
-import { rejects } from 'assert';
-import { MongoClient, ObjectId, Document, MongoError } from 'mongodb';
-import { createImportSpecifier } from 'typescript';
+import {
+  MongoClient,
+  ObjectId,
+  Document,
+  MongoError,
+  DeleteResult,
+} from 'mongodb';
 import { CollectionMap, KeyValuePair, mongoConnectorConfig } from './models';
 
-
 // mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]
-
 class MongoDBConnector {
   public dbName: string;
   public port: number;
@@ -17,8 +19,7 @@ class MongoDBConnector {
   private password: string;
   private host: string;
   private client: MongoClient;
-  
-  
+
   constructor(req: Request, config: mongoConnectorConfig) {
     this.baseURL = config.baseURL;
     this.dbName = config.databaseName;
@@ -27,164 +28,195 @@ class MongoDBConnector {
     this.user = config.user;
     this.password = config.password;
     this.host = config.host || 'localhost';
-    this.port = config.port || 20717;
+    this.port = config.port || 27017;
     this.connectionString = `mongodb://${this.host}:${this.port}/?maxPoolSize=20&w=majority`;
     this.client = new MongoClient(this.connectionString);
   }
 
+  // // Returns 1 record
+  // public async getById(id: ObjectId, collection: string) {
+  //   try {
+  //     await this.connect();
+  //     const collectionName = this.collectionsMap[collection]
+  //     if (collectionName) {
+  //       let col = collectionName.toLowerCase() as string;
+  //       const response = await this.client.db(this.dbName).collection(col).findOne({_id: new ObjectId(id)});
+  //       if (response) {
+  //         return response;
+  //       }
+  //     }
+  //     return null;
+  //   } catch(e) {
+  //     console.log(e);
+  //   } finally {
+  //     await this.close();
+  //   }
+  //   return null;
+  // }
 
-// // Returns 1 record
-// public async getById(id: ObjectId, collection: string) {
-//   try {
-//     await this.connect();
-//     const collectionName = this.collectionsMap[collection]
-//     if (collectionName) {
-//       let col = collectionName.toLowerCase() as string;
-//       const response = await this.client.db(this.dbName).collection(col).findOne({_id: new ObjectId(id)});
-//       if (response) {
-//         return response;
-//       }
-//     }
-//     return null;
-//   } catch(e) {
-//     console.log(e);
-//   } finally {
-//     await this.close();
-//   }
-//   return null;
-// }
+  public async getEntireCollection(
+    collection: string
+  ): Promise<Document[] | null> {
+    try {
+      await this.connect();
+      const collectionName = this.getCollectionName(collection);
+      if (collectionName) {
+        let col = collectionName.toLowerCase();
+        const response = await this.client
+          .db(this.dbName)
+          .collection(col)
+          .find()
+          .toArray();
+        if (response.length > 0) {
+          return response;
+        }
+      }
+    } catch (e: any) {
+      console.log(e.message);
+    } finally {
+      await this.close();
+    }
+    return null;
+  }
 
+  /**
+   *
+   * @param collection string
+   * @param id Mongo ObjectId
+   * @returns DeleteResult {acknowleged: boolean, deletedCount: number}
+   */
+  public async deleteOneItem(
+    collection: string,
+    id: ObjectId
+  ): Promise<DeleteResult | undefined> {
+    try {
+      await this.connect();
+      const collectionName = this.getCollectionName(collection) as string;
 
-public async getEntireCollection(collection: string): Promise<Document[]|null> {
-  try {
-    await this.connect();
-    const collectionName = this.getCollectionName(collection);
-    if (collectionName) {
-      let col = collectionName.toLowerCase();
-      const response = await this.client.db(this.dbName).collection(col).find().toArray();
-      if (response.length > 0) {
+      if (collectionName === undefined) {
+        throw new Error();
+      }
+
+      let col = collectionName.toLowerCase() as string;
+      if (col) {
+        const response = await this.client
+          .db(this.dbName)
+          .collection(col)
+          .deleteOne({ _id: new ObjectId(id) });
         return response;
       }
+    } catch (e: any) {
+      throw new Error('DELETE ONE ERROR');
+    } finally {
+      await this.close();
     }
-  } catch(e:any) {
-    console.log('GET ENTIRE COLLECTION ERROR')
-    console.log(e.message);
-  } finally {
-    await this.close();
   }
-  return null;
-}
 
-// public async deleteOneItem(id: ObjectId, collection:string) {
-//   try {
-//     await this.connect();
-//     const collectionName = this.getCollectionName(collection) as string;
-//     if (collectionName) {
-//       let col = collectionName.toLowerCase() as string;
-//       const response = await this.client.db(this.dbName).collection(col).deleteOne({_id: new ObjectId(id)});
-//       if (response) {
-//         return response;
-//       }
-//     }
-//     return null;
-//   } catch(e) {
-//     console.log(e);
-//   } finally {
-//     await this.close();
-//   }
-//   return null;
-// }
+  // public async updateOneItem(id: ObjectId, collection: string, payload:any): Promise<any | null> {
+  //   try {
+  //     await this.connect();
+  //     const col = this.getCollectionName(collection) as string;
+  //     const response = await this.client.db(this.dbName).collection(col).findOneAndUpdate({_id: new ObjectId(id)}, {$set: payload}, {returnDocument: 'after'});
+  //     if (response.ok === 1) {
+  //       return response.value;
+  //     }
+  //   } catch(e) {
+  //     console.log(e);
+  //     return null;
+  //   } finally {
+  //     await this.close();
+  //   }
+  //   return null;
+  // }
 
-// public async updateOneItem(id: ObjectId, collection: string, payload:any): Promise<any | null> {
-//   try {
-//     await this.connect();
-//     const col = this.getCollectionName(collection) as string;
-//     const response = await this.client.db(this.dbName).collection(col).findOneAndUpdate({_id: new ObjectId(id)}, {$set: payload}, {returnDocument: 'after'});
-//     if (response.ok === 1) {
-//       return response.value;
-//     }
-//   } catch(e) {
-//     console.log(e);
-//     return null;
-//   } finally {
-//     await this.close();
-//   }
-//   return null;
-// }
-
-/*
+  /*
   @param collection name: string
   @param payload: object to be inserted
-  @return Newly inserted item or null
+  @return Newly inserted item or null. Throws error if collection not found
 */
-public async insertOneItem(collection:string, payload: any): Promise<any | null> {
-  try {
-    await this.connect();
-    const col = this.getCollectionName(collection);
-    if (!col) {
-      return null;
+  public async insertOneItem(
+    collection: string,
+    payload: any
+  ): Promise<any | null> {
+    try {
+      await this.connect();
+      const col = this.getCollectionName(collection);
+
+      if (col === undefined) {
+        throw new Error('COLLECTION NAME ERROR');
+      }
+
+      const result = await this.client
+        .db(this.dbName)
+        .collection(col)
+        .insertOne(payload);
+
+      if (result?.acknowledged && result?.insertedId) {
+        const insertedItem = await this.client
+          .db(this.dbName)
+          .collection(col)
+          .findOne({ _id: new ObjectId(result.insertedId) });
+        return insertedItem;
+      }
+    } catch (e: any) {
+      throw new Error('COLLECTION NAME ERROR CAUGHT');
+    } finally {
+      await this.close();
     }
-    const result = await this.client.db(this.dbName).collection(col).insertOne(payload);
-    if (result?.acknowledged && result?.insertedId) {
-      const insertedItem = await this.client.db(this.dbName).collection(col).findOne({_id: new ObjectId(result.insertedId)});
-      return insertedItem;
+  }
+
+  public async dropCollection(collectionName: string): Promise<boolean | null> {
+    try {
+      await this.connect();
+      const col = this.getCollectionName(collectionName);
+      const result = await this.client.db(this.dbName).collection(col).drop();
+      return result;
+    } catch (e: any) {
+      // console.log('DROP COLLECTION');
+    } finally {
+      await this.close();
     }
-  } catch(e:any) {
-    console.log('INSERT ONE ERROR')
-  } finally {
-    await this.close();
+    return null;
   }
-}
 
-public async dropCollection(collectionName: string): Promise<boolean|null> {
-  try {
-    await this.connect();
-    const col = this.getCollectionName(collectionName);
-    const result = await this.client.db(this.dbName).collection(col).drop();
-    return result;
-  } catch(e: any) {
-    console.log('DROP COLLECTION ERROR')
-  } finally {
-    await this.close();
+  public async deleteDatabase() {
+    await this.client.db(this.dbName).dropDatabase();
   }
-  return null;
-}
 
-public async deleteDatabase() {
-  await this.client.db(this.dbName).dropDatabase();
-}
+  public getCollectionsMap = (): CollectionMap => this.collectionsMap;
 
+  public getDatabaseName = (): string => this.dbName;
 
-public getCollectionsMap = () =>  this.collectionsMap;
+  private async connect(): Promise<void> {
+    await this.client.connect();
+  }
 
-public getDatabaseName = () => this.dbName;
+  public async close(): Promise<void> {
+    await this.client.close();
+  }
 
-private async connect() {
-  await this.client.connect();
-}
+  // Get collection name from collection map
+  private getCollectionName(collectionName: string): string {
+    try {
+      return this.collectionsMap[collectionName.toUpperCase()];
+    } catch (e) {
+      console.log('GET COLLECCTION ERROR');
+      throw new Error('COLLECTION NAME ERROR');
+    }
+  }
 
-private async close(): Promise<void> {
-  await this.client.close();
-}
-
-
-// Get collection name from collection map
-private getCollectionName(collectionName:string): string {
-  return this.collectionsMap[collectionName.toUpperCase()];
-}
-
-
-// Collection map of UPPER KEY : lower name
-private getCollectionNameMap(collections: string[]): CollectionMap {
-  const collectionNameMap = collections.reduce((accu:CollectionMap, value:string, idx:number) => {
-    const key = value.toUpperCase();
-    accu[key] = value.toLowerCase();
-    return accu;
-  }, {})
-  return collectionNameMap;
-}
+  // Collection map of UPPER KEY : lower name
+  private getCollectionNameMap(collections: string[]): CollectionMap {
+    const collectionNameMap = collections.reduce(
+      (accu: CollectionMap, value: string, idx: number) => {
+        const key = value.toUpperCase();
+        accu[key] = value.toLowerCase();
+        return accu;
+      },
+      {}
+    );
+    return collectionNameMap;
+  }
 }
 
 export default MongoDBConnector;
-
-
--- src/playground.ts
+++ src/playground.ts
@@ -2,16 +2,13 @@ import MongoDbConnector from './index';
 import { mongoConnectorConfig } from './models';
 
 const config: mongoConnectorConfig = {
-    baseURL: "mongodb://127.0.0.1:27017",
-    databaseName: "test",
-    collections: ["kittens"],
-    timeout: 1000,
-    headers: {},
-    user: 'test',
-    password: "",
-    host: 'mongodb',
-}
-
-const connection = new MongoDbConnector({} as Request, config);
-
-console.log(connection.getDatabaseName());
+  baseURL: 'mongodb://127.0.0.1:27017',
+  databaseName: 'test',
+  collections: ['kittens'],
+  timeout: 1000,
+  headers: {},
+  user: 'test',
+  password: '',
+  host: 'mongodb',
+  port: 20717,
+};
